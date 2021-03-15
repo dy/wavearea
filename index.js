8f070d836a83f5de3276979ed6c6cb8442130a96
dy <df.creative@gmail.com>
2021-03-15 12:40:05 -0400

    Implement recording

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ import settingsIcon from './asset/settings.svg';
 
 export default class Wavearea {
   paused = true
+  chunks = []
+  timeslice = null
+
   constructor (textarea, o={}) {
     // DOM
     this.textarea = textarea
+    this.textarea.style.lineHeight = 1
 
     ;(this.primaryControls = document.createElement('div'))
     .innerHTML = `
@@ -31,12 +35,12 @@ export default class Wavearea {
 
     // fix position
     this.textarea.addEventListener('focus', e => {
-      reposition(this.textarea, this.primaryControls, {margin: -32, position: 'bottom-start'})
-      reposition(this.textarea, this.secondaryControls, {margin: -32, position: 'bottom-end'})
+      reposition(this.textarea, this.primaryControls, {margin: -24, position: 'bottom-start'})
+      reposition(this.textarea, this.secondaryControls, {margin: -24, position: 'bottom-end'})
     });
     const resizeObserver = new ResizeObserver(() => {
-      reposition(this.textarea, this.primaryControls, {margin: -32, position: 'bottom-start'})
-      reposition(this.textarea, this.secondaryControls, {margin: -32, position: 'bottom-end'})
+      reposition(this.textarea, this.primaryControls, {margin: -24, position: 'bottom-start'})
+      reposition(this.textarea, this.secondaryControls, {margin: -24, position: 'bottom-end'})
     });
     resizeObserver.observe(this.textarea);
 
@@ -46,113 +50,100 @@ export default class Wavearea {
     // interactions
     this.playButton.addEventListener('click', e => this.paused ? this.play() : this.pause())
     this.recordButton.addEventListener('click', e => this.paused ? this.record() : this.pause())
+
+    // audio
+    this.playback = document.createElement('audio')
   }
+
+  async initRecorder() {
+    let stream = this.stream = await navigator.mediaDevices.getUserMedia({audio: true})
+
+    const audioContext = new AudioContext();
+    const source = audioContext.createMediaStreamSource(stream);
+    const analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    source.connect(analyser);
+    const dataArray = new Float32Array(bufferLength);
+
+    this.timeslice = analyser.fftSize/audioContext.sampleRate
+
+    this.recorder = new MediaRecorder(stream);
+    this.recorder.ondataavailable = (e) => {
+      // no need to turn data into array buffers, unless we're able to read them instead of Web-Audio-API
+      this.chunks.push(e.data)
+
+      // FIXME: capture header
+      if (this.chunks.length > 2) {
+        // FIXME: is that possible to read data from audio chunks?
+        analyser.getFloatTimeDomainData(dataArray);
+        let ssum = 0
+        for (let i = 0; i < dataArray.length; i++) ssum += dataArray[i] * dataArray[i]
+        const rms = Math.sqrt(ssum / dataArray.length)
+        this.textarea.append( String.fromCharCode(0x0100 + Math.floor(rms * 100)))
+      }
+    }
+  }
+
   pause() {
     this.paused = true
     this.recordButton.innerHTML = `${recordIcon}`
     this.playButton.innerHTML = `${playIcon}`
+
+    this.recorder.stop()
   }
+
   play() {
     this.paused = false
     this.playButton.innerHTML = `${pauseIcon}`
+
+    const from = Math.max(wavearea.selectionStart, 2), to = wavearea.selectionEnd
+    const frag = from === to ? chunks.slice(from) : chunks.slice(from, to)
+    // NOTE: initial 2-3 chunks are required for playback source validity
+    // these chunks can be seen on waveform
+    // FIXME: ideally delegate to webworker?
+    frag.unshift(chunks[0], chunks[1])
+    let blob = new Blob(frag, { type: recorder.mimeType })
+    playback.src = window.URL.createObjectURL(blob)
+
+    // FIXME: chrome is picky for min chunk length, it disregards short chunks, that's why we can only do playback.src
+    // maybe that's possible to workaround by merging chunks, who knows
+    // create playback stream - anything flushed into sourceBuffer will be played
+    // var mediaSource = new MediaSource();
+    // playback.src = URL.createObjectURL(mediaSource);
+    // var sourceopen = new Promise(resolve => console.log('sourceopen') || mediaSource.addEventListener('sourceopen', resolve));
+
+    // console.log('add chunks', wavearea.selectionStart, queue)
+    // await sourceopen
+    // const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+    // sourceBuffer.onerror = (...e) => console.log('sourcebuffer error', ...e)
+    // sourceBuffer.mode = 'sequence'
+
+    // // sourceBuffer.onupdate = e => console.log('update',e)
+    // sourceBuffer.onupdateend = e => {
+    //   // console.log('updateend', sourceBuffer)
+    //   // sourceBuffer.timestampOffset += chunkDuration
+    //   flush()
+    // }
+
+    // const flush = () => {
+    //   if (sourceBuffer.updating) return
+    //   buf = queue.shift()
+    //   if (buf) sourceBuffer.appendBuffer(buf)
+    // }
+
+    // flush()
   }
-  record() {
+
+  async record() {
+    if (!this.stream) await this.initRecorder()
+
     this.paused = false
     this.recordButton.innerHTML = `${pauseIcon}`
+
+    // recorder.start()
+    this.recorder.start(1000 * this.timeslice)
   }
 }
 
-// const wavearea = document.getElementById('wavearea')
-// const playback = document.getElementById('playback')
 
-// navigator.mediaDevices.getUserMedia({audio: true}).then(record)
-
-// const sleep = (delay) => new Promise(resolve => setTimeout(resolve, delay))
-
-// let chunks = [], mediaRecorder
-// function record (stream) {
-//   const audioCtx = new AudioContext();
-//   const source = audioCtx.createMediaStreamSource(stream);
-//   const analyser = audioCtx.createAnalyser();
-//   analyser.fftSize = 2048;
-//   const bufferLength = analyser.frequencyBinCount;
-//   const dataArray = new Float32Array(bufferLength);
-//   source.connect(analyser);
-//   // chunkDuration = analyser.fftSize/audioCtx.sampleRate
-
-//   let stop = false
-//   setTimeout(() => {
-//     console.log('stop')
-//     mediaRecorder.stop()
-//     stop = true
-//   }, 5000)
-
-//   mediaRecorder = new MediaRecorder(stream);
-//   mediaRecorder.ondataavailable = function(e) {
-//     e.data.arrayBuffer().then(buf => {
-//       chunks.push(buf);
-//       console.log('dataavailable',buf)
-//       draw()
-//       if (stop) {
-//         console.log('dataavailable stop', ...chunks)
-//       }
-//     })
-//   }
-//   // mediaRecorder.start()
-//   mediaRecorder.start(1000 * analyser.fftSize / audioCtx.sampleRate)
-
-//   function draw() {
-//     analyser.getFloatTimeDomainData(dataArray);
-//     let ssum = 0
-//     for (let i = 0; i < dataArray.length; i++) ssum += dataArray[i] * dataArray[i]
-//     const rms = Math.sqrt(ssum / dataArray.length)
-//     wavearea.append( String.fromCharCode(0x0100 + Math.floor(rms * 100)))
-//   }
-// }
-
-
-// // create playback stream - anything flushed into sourceBuffer will be played
-// var mediaSource = new MediaSource();
-// playback.src = URL.createObjectURL(mediaSource);
-// var sourceopen = new Promise(resolve => console.log('sourceopen') || mediaSource.addEventListener('sourceopen', resolve));
-
-// // play clicked chunk
-// wavearea.addEventListener('click', caretchange)
-// // wavearea.addEventListener('input', caretchange)
-// // wavearea.addEventListener('select', caretchange)
-// // wavearea.addEventListener('keyup', caretchange)
-
-// // playback.addEventListener('ended', () => playback.pause())
-// async function caretchange(){
-//   const from = Math.max(wavearea.selectionStart, 2), to = wavearea.selectionEnd
-//   const frag = from === to ? chunks.slice(from) : chunks.slice(from, to)
-//   // NOTE: initial 2-3 chunks are required for playback source validity
-//   // these chunks can be seen on waveform
-//   // FIXME: ideally delegate to webworker?
-//   frag.unshift(chunks[0], chunks[1])
-//   let blob = new Blob(frag, { type: mediaRecorder.mimeType })
-//   playback.src = window.URL.createObjectURL(blob)
-
-//   // FIXME: chrome is picky for min chunk length, it disregards short chunks, that's why we can only do playback.src
-//   // maybe that's possible to workaround by merging chunks, who knows
-//   // console.log('add chunks', wavearea.selectionStart, queue)
-//   // await sourceopen
-//   // const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-//   // sourceBuffer.onerror = (...e) => console.log('sourcebuffer error', ...e)
-//   // sourceBuffer.mode = 'sequence'
-
-//   // // sourceBuffer.onupdate = e => console.log('update',e)
-//   // sourceBuffer.onupdateend = e => {
-//   //   // console.log('updateend', sourceBuffer)
-//   //   // sourceBuffer.timestampOffset += chunkDuration
-//   //   flush()
-//   // }
-
-//   // const flush = () => {
-//   //   if (sourceBuffer.updating) return
-//   //   buf = queue.shift()
-//   //   if (buf) sourceBuffer.appendBuffer(buf)
-//   // }
-
-//   // flush()
-// }
