18d6da04a6ccaf0ddf5fa03f87f0d91005b3b009
Dmitry Iv <df.creative@gmail.com>
2023-01-10 23:52:08 -0500

    Rebuild, key segments to avoid huge rerenders

---
+++ dist/chunk-YFDESRXI.js
@@ -0,0 +1,2 @@
+var d=Object.defineProperty;var e=(b,a)=>()=>(a||b((a={exports:{}}).exports,a),a.exports),f=(b,a)=>{for(var c in a)d(b,c,{get:a[c],enumerable:!0})};export{e as a,f as b};
+//# sourceMappingURL=chunk-YFDESRXI.js.map
